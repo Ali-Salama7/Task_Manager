72896e5b3f58a2ed667f5cd83abc77d05f4765df
Ali Salama <alisalamadev@gmail.com>
2026-06-18 21:41:03 +0300

    add user entity

---
+++ src/main/java/com/example/taskmanager/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.taskmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
